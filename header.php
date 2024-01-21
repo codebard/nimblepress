4f8566f9c3ff68fc0a0f3d48610177763a2704cf
Codebard <root@thinking17.mindonthenet.com>
2024-01-21 03:08:52 +0000

    Testing inlining all css.

--- header.php
+++ header.php
@@ -16,7 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+<style>
+    <?php include get_template_directory() . '/style.css'; ?>
+</style>
 	<?php wp_head(); ?>
 </head>
 
